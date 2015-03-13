4915afd41de31f7c261081c2db870d054fe54445
Chris Swierczewski <cswiercz@gmail.com>
2015-03-13 09:50:15 -0700

    Change tests to have binary output
    Tests which pass should print "1". Tests that fail should
    print "0". A future update should only print if the test
    fails. Consider incorporating a C++ test suite for
    practice.

--- test/test.cpp
+++ test/test.cpp
@@ -7,19 +7,19 @@ using std::endl;
 int main(void)
 {
   Rational<int> r = {1,2};
-  cout << r.print() << endl;
+  cout << "r = " << r.print() << endl;
 
   Rational<int> s = {3,5};
   Rational<int> t = r*s;
-  cout << s.print() << endl;
-  cout << t.print() << endl;
+  cout << "s = " << s.print() << endl;
+  cout << "t = " << t.print() << endl;
 
   cout << endl << "Test Comparison:" << endl;
   cout << (r < s) << endl;
-  cout << (r > s) << endl;
-  cout << (r == s) << endl;
+  cout << !(r > s) << endl;
+  cout << !(r == s) << endl;
   cout << (r <= s) << endl;
-  cout << (r >= s) << endl;
+  cout << !(r >= s) << endl;
   cout << (r == r) << endl;
 
   cout << endl << "Test Multiplication:" << endl;
@@ -27,9 +27,9 @@ int main(void)
   cout << ((r * 1) == r) << endl;
 
   cout << endl << "Test Addition:" << endl;
-  cout << (r + s).print() << endl;
-  cout << (r + 1).print() << endl;
-  cout << (1 + r).print() << endl;
+  cout << (r + s == Rational<int>(11,10)) << endl;
+  cout << (r + 1 == Rational<int>(3,2)) << endl;
+  cout << (1 + r == Rational<int>(3,2)) << endl;
 
   return 0;
 }
